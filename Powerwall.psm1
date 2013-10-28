c2f13ae1111007d39359498cf05048be5d4e756f
John Moore <melderan@gmail.com>
2013-10-28 05:59:15 -0500

    Add Powerwall.psm1

---
+++ Powerwall.psm1
@@ -0,0 +1,42 @@
+﻿function Get-FirewallRule {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$False,Position=1)]
+        [string]$Name
+    )
+    if($Name -eq ''){$Name='all'}
+    netsh advfirewall firewall show rule name=$Name
+}
+
+function Update-FirewallRule {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Name,
+
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$RemoteIPs
+    )
+    
+    Write-Verbose '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    $RemoteIPField = (Get-FirewallRule $Name) | ? {$_.StartsWith('RemoteIP')}
+    Write-Verbose $RemoteIPField
+    Write-Verbose '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    $RemoteIPList = $RemoteIPField.split()[-1]
+    Write-Verbose $RemoteIPList
+
+    if($RemoteIPList -eq 'Any') {
+        Write-Verbose 'No Remote IPs previously set'
+        $RemoteIPList = ''
+        Write-Verbose 'Set field to ""'
+    } else {
+        $RemoteIPList += ','
+    }
+    
+    Write-Verbose '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    $NewRemoteIPList = $RemoteIPList + $RemoteIPs
+    Write-Verbose $NewRemoteIPList
+    Write-Verbose '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    
+    netsh advfirewall firewall set rule name=$Name new remoteip=$NewRemoteIPList
+}
